69847e1645ec5c0ad4f5b89bee690d14e8bdd7cb
nikm <nikm.2099@gmail.com>
2024-10-25 17:39:36 +0530

    Update4
    layout.jsx change title DDDEinblasdämmung

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,7 +2,7 @@ import type { Metadata } from "next";
 import "./globals.css";
 
 export const metadata: Metadata = {
-  title: "My Photo Gallery",
+  title: "DDDEinblasdämmung",
 };
 
 export default function RootLayout({
